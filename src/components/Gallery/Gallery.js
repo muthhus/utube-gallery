966ca66f3c5d716d4e8d128f3a511a3820016831
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-12-04 12:01:19 +0000

    retrived static contents list

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,14 +1,19 @@
 import React from 'react';
+
+import Aux from '../../hoc/Aux';
 import  './Gallery.css';
 
 const gallery = (props) => {
     return (
-        <div className="Gallery">
+        <Aux>
+            <div className="Gallery">
                 <p onClick={props.getTitle}>Title: {props.title}</p>
                 <p>Description: {props.description}</p>
                 <p>{props.children}</p>
                 <p>Rating: {props.rating} <input type="text" onChange={props.review} value={props.rating} /></p>
-        </div>
+            </div>
+        </Aux>
+        
     )
 };
 
